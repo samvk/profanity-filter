1603c6fbf75ddc9878e21d796383a217464169fb
Sam Kauffman <samvnkauffman@gmail.com>
2019-01-09 00:04:09 -0500

    convert to Sets

---
+++ language/no.js
@@ -0,0 +1,14 @@
+module.exports = new Set([
+    "drittsekk",
+    "fitte",
+    "jævla",
+    "kuk",
+    "kukene",
+    "kuker",
+    "nigger",
+    "pikk",
+    "sotrør",
+    "ståpikk",
+    "ståpikkene",
+    "ståpikker",
+]);
---
+++ language/tlh.js
@@ -0,0 +1,5 @@
+module.exports = new Set([
+    "ghuy'cha'",
+    "QI'yaH",
+    "Qu'vatlh",
+]);
